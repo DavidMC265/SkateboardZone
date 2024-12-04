589a1b49f731871d34669630ce34b6964f63a999
DavidMC265 <d.mayorga489@gmail.com>
2024-12-04 00:06:13 -0600

    Commit

---
+++ SkateboardSim/Intermediate/Build/Win64/UnrealEditor/Inc/SkateboardSim/UHT/WaypointManager.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Waypoint/WaypointManager.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SKATEBOARDSIM_WaypointManager_generated_h
+#error "WaypointManager.generated.h already included, missing '#pragma once' in WaypointManager.h"
+#endif
+#define SKATEBOARDSIM_WaypointManager_generated_h
+
+#define FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointManager_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAWaypointManager(); \
+	friend struct Z_Construct_UClass_AWaypointManager_Statics; \
+public: \
+	DECLARE_CLASS(AWaypointManager, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SkateboardSim"), NO_API) \
+	DECLARE_SERIALIZER(AWaypointManager)
+
+
+#define FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointManager_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	AWaypointManager(AWaypointManager&&); \
+	AWaypointManager(const AWaypointManager&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AWaypointManager); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AWaypointManager); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AWaypointManager) \
+	NO_API virtual ~AWaypointManager();
+
+
+#define FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointManager_h_9_PROLOG
+#define FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointManager_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointManager_h_12_INCLASS_NO_PURE_DECLS \
+	FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointManager_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SKATEBOARDSIM_API UClass* StaticClass<class AWaypointManager>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointManager_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ SkateboardSim/Intermediate/Build/Win64/UnrealEditor/Inc/SkateboardSim/UHT/WaypointSpawnPoint.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Waypoint/WaypointSpawnPoint.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SKATEBOARDSIM_WaypointSpawnPoint_generated_h
+#error "WaypointSpawnPoint.generated.h already included, missing '#pragma once' in WaypointSpawnPoint.h"
+#endif
+#define SKATEBOARDSIM_WaypointSpawnPoint_generated_h
+
+#define FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointSpawnPoint_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAWaypointSpawnPoint(); \
+	friend struct Z_Construct_UClass_AWaypointSpawnPoint_Statics; \
+public: \
+	DECLARE_CLASS(AWaypointSpawnPoint, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SkateboardSim"), NO_API) \
+	DECLARE_SERIALIZER(AWaypointSpawnPoint)
+
+
+#define FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointSpawnPoint_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	AWaypointSpawnPoint(AWaypointSpawnPoint&&); \
+	AWaypointSpawnPoint(const AWaypointSpawnPoint&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AWaypointSpawnPoint); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AWaypointSpawnPoint); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AWaypointSpawnPoint) \
+	NO_API virtual ~AWaypointSpawnPoint();
+
+
+#define FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointSpawnPoint_h_9_PROLOG
+#define FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointSpawnPoint_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointSpawnPoint_h_12_INCLASS_NO_PURE_DECLS \
+	FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointSpawnPoint_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SKATEBOARDSIM_API UClass* StaticClass<class AWaypointSpawnPoint>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_SkateboardSim_Source_SkateboardSim_Public_Waypoint_WaypointSpawnPoint_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
